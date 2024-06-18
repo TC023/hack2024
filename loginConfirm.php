5f373f11c0aa0e04c1a6929d6ba55c3d0b4b1970
Alonso Lopez Baez <a01736813@tec.mx>
2024-06-18 16:36:54 -0600

    commit generico

--- loginConfirm.php
+++ loginConfirm.php
@@ -9,10 +9,11 @@
 	
 	$sql = "SELECT * FROM users WHERE email = '$email' AND pass = '$contrasena'";
 	$stmt = $pdo->query($sql);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 	if ($stmt->rowCount() === 1 OR $_POST["isAdmin"] ?? 0) {
         echo 'Éxito, entrando...';
+        header("refresh:1; url=cursos.php?id=".$result["user_id"]);
 	} else {
         echo 'Inicio de sesión fallido, regresando...';
-        header("refresh:3; url=cursos.html");
-        
+        header("refresh:3; url=index.php");
 	}
